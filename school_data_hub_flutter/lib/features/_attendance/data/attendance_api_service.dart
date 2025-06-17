c821d4f94bcb4389cb6f13d1703ecaf7f1905544
dabblingwithcode <nacho.dominguis@proton.me>
2025-06-17 23:26:41 +0200

    fix

--- school_data_hub_flutter/lib/features/_attendance/data/attendance_api_service.dart
+++ school_data_hub_flutter/lib/features/_attendance/data/attendance_api_service.dart
@@ -5,23 +5,27 @@ import 'package:school_data_hub_flutter/core/session/hub_session_manager.dart';
 import 'package:school_data_hub_flutter/features/school_calendar/domain/school_calendar_manager.dart';
 import 'package:watch_it/watch_it.dart';
 
-final _client = di<Client>();
-final _session = di<HubSessionManager>();
-final _schoolCalendarManager = di<SchoolCalendarManager>();
-final _notificationService = di<NotificationService>();
-
 class AttendanceApiService {
-//- fetch all missed classes -//
+  final _client = di<Client>();
+
+  final _session = di<HubSessionManager>();
+
+  final _schoolCalendarManager = di<SchoolCalendarManager>();
+
+  final _notificationService = di<NotificationService>();
+
+//- fetch all missed schooldays -//
 
   Future<List<MissedSchoolday>?> fetchAllMissedSchooldayes() async {
     final missedSchooldays = await ClientHelper.apiCall(
       call: () => _client.missedSchoolday.fetchAllMissedSchooldays(),
       errorMessage: 'Fehler beim Laden der Fehlzeiten',
     );
+
     return missedSchooldays;
   }
 
-  //- fetch missed classes for a date -//
+  //- fetch missed schooldays for a date -//
 
   Future<List<MissedSchoolday>?> fetchMissedSchooldayesOnASchoolday(
       DateTime schoolday) async {
@@ -30,10 +34,11 @@ class AttendanceApiService {
           _client.missedSchoolday.fetchMissedSchooldaysOnASchoolday(schoolday),
       errorMessage: 'Fehler beim Laden der Fehlzeiten',
     );
+
     return missedSchooldays;
   }
 
-  //- post new class -//
+  //- post new missed schoolday -//
 
   Future<MissedSchoolday?> postMissedSchoolday({
     required int pupilId,
@@ -47,6 +52,7 @@ class AttendanceApiService {
     ContactedType? contactedType,
   }) async {
     final schoolday = _schoolCalendarManager.getSchooldayByDate(date);
+
     final MissedSchoolday missedSchoolday = MissedSchoolday(
         missedType: missedType,
         unexcused: unexcused ?? false,
@@ -61,10 +67,11 @@ class AttendanceApiService {
       call: () => _client.missedSchoolday.postMissedSchoolday(missedSchoolday),
       errorMessage: 'Fehler beim Eintragen der Fehlzeit',
     );
+
     return missedSchooldayResponse;
   }
 
-  //- post a list of missed classes -//
+  //- post a list of missed schooldays -//
 
   Future<List<MissedSchoolday>?> postMissedSchooldayList(
       {required List<MissedSchoolday> missedSchooldays}) async {
@@ -73,10 +80,11 @@ class AttendanceApiService {
           _client.missedSchoolday.postMissedSchooldays(missedSchooldays),
       errorMessage: 'Fehler beim Eintragen der Fehlzeiten',
     );
+
     return missedSchooldaysList;
   }
 
-  //- patch a missed class -//
+  //- update a missed schoolday -//
 
   Future<MissedSchoolday?> updateMissedSchoolday({
     required MissedSchoolday missedSchooldayToUpdate,
@@ -86,10 +94,11 @@ class AttendanceApiService {
           .updateMissedSchoolday(missedSchooldayToUpdate),
       errorMessage: 'Fehler beim Aktualisieren der Fehlzeit',
     );
+
     return missedSchoolday;
   }
 
-  //- delete missed class -//
+  //- delete missed schoolday -//
 
   Future<bool?> deleteMissedSchoolday(int pupilId, int schooldayId) async {
     _notificationService.apiRunning(true);
@@ -98,6 +107,7 @@ class AttendanceApiService {
           _client.missedSchoolday.deleteMissedSchoolday(pupilId, schooldayId),
       errorMessage: 'Fehler beim Löschen der Fehlzeit',
     );
+
     return success;
   }
 }
