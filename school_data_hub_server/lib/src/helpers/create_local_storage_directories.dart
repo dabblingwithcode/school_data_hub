ef6347a789ce1df220311d5b9752c2637a7aa1eb
dabblingwithcode <nacho.dominguis@proton.me>
2025-08-26 15:17:44 +0200

    debug: try to log the server

--- school_data_hub_server/lib/src/helpers/create_local_storage_directories.dart
+++ school_data_hub_server/lib/src/helpers/create_local_storage_directories.dart
@@ -6,41 +6,65 @@ import 'package:path/path.dart' as p;
 final _log = Logger('CreateLocalStorageDirectories');
 
 void createLocalStorageDirectories() {
-  // Get the current working directory
-  final serverDirPath = Directory.current.path;
+  IOSink? logSink;
+  // Configure file logging
+  try {
+    logSink = _setupFileLogging();
+    // Get the current working directory
+    final serverDirPath = Directory.current.path;
+    _log.fine('Current path is [$serverDirPath] .');
+    // Create the directories for local storage
+    final storageDir = Directory(p.join(serverDirPath, 'storage'));
+    _log.fine('Storage path is [${storageDir.path}] .');
+    // If the storage directory does not exist,
+    // the subdirectories do not exist either
+    if (storageDir.existsSync()) {
+      _log.fine('Storage directory [${storageDir.path}] already exists.');
+    } else {
+      _log.warning(
+        'Storage directory not found in [${storageDir.path}] - creating...',
+      );
+      storageDir.createSync(recursive: true);
+      final publicDir = Directory(p.join(storageDir.path, 'public'));
+      final serverpodDir = Directory(p.join(publicDir.path, 'serverpod'));
+      final userImagesDir = Directory(p.join(serverpodDir.path, 'user_images'));
+      final privateDir = Directory(p.join(storageDir.path, 'private'));
+      final tempDir = Directory(p.join(storageDir.path, 'temp'));
+      final authsDir = Directory(p.join(privateDir.path, 'auths'));
+      final avatarsDir = Directory(p.join(privateDir.path, 'avatars'));
+      final documentsDir = Directory(p.join(privateDir.path, 'documents'));
+      final evertsDir = Directory(p.join(privateDir.path, 'events'));
 
-  // Create the directories for local storage
-  final storageDir = Directory(p.join(serverDirPath, 'storage'));
-
-  // If the storage directory does not exist,
-  // the subdirectories do not exist either
-  if (storageDir.existsSync()) {
-    _log.fine('Storage directory [${storageDir.path}] already exists.');
-  } else {
-    _log.warning(
-      'Storage directory not found in [${storageDir.path}] - creating...',
-    );
-    storageDir.createSync(recursive: true);
-    final publicDir = Directory(p.join(storageDir.path, 'public'));
-    final serverpodDir = Directory(p.join(publicDir.path, 'serverpod'));
-    final userImagesDir = Directory(p.join(serverpodDir.path, 'user_images'));
-    final privateDir = Directory(p.join(storageDir.path, 'private'));
-    final tempDir = Directory(p.join(storageDir.path, 'temp'));
-    final authsDir = Directory(p.join(privateDir.path, 'auths'));
-    final avatarsDir = Directory(p.join(privateDir.path, 'avatars'));
-    final documentsDir = Directory(p.join(privateDir.path, 'documents'));
-    final evertsDir = Directory(p.join(privateDir.path, 'events'));
-
-    publicDir.createSync(recursive: true);
-    serverpodDir.createSync(recursive: true);
-    userImagesDir.createSync(recursive: true);
-    tempDir.createSync(recursive: true);
-    authsDir.createSync(recursive: true);
-    avatarsDir.createSync(recursive: true);
-    documentsDir.createSync(recursive: true);
-    evertsDir.createSync(recursive: true);
-    _log.fine(
-      'Storage directories created successfully in [${storageDir.path}]',
-    );
+      publicDir.createSync(recursive: true);
+      serverpodDir.createSync(recursive: true);
+      userImagesDir.createSync(recursive: true);
+      tempDir.createSync(recursive: true);
+      authsDir.createSync(recursive: true);
+      avatarsDir.createSync(recursive: true);
+      documentsDir.createSync(recursive: true);
+      evertsDir.createSync(recursive: true);
+      _log.fine(
+        'Storage directories created successfully in [${storageDir.path}]',
+      );
+    }
+  } finally {
+    // Dispose of the log file stream
+    logSink?.flush();
+    logSink?.close();
   }
+}
+
+IOSink _setupFileLogging() {
+  final logFile = File('storage_directories.log');
+  final logSink = logFile.openWrite(mode: FileMode.append);
+
+  Logger.root.onRecord.listen((record) {
+    if (record.loggerName == 'CreateLocalStorageDirectories') {
+      final logEntry =
+          '${record.time}: ${record.level.name}: ${record.message}\n';
+      logSink.write(logEntry);
+    }
+  });
+
+  return logSink;
 }
