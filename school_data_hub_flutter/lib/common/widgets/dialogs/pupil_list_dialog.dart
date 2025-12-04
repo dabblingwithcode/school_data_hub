1f41a6bd880f92dbbd9e6e9921c1d86bf5d9d45a
dabblingwithcode <nacho.dominguis@proton.me>
2025-12-04 14:13:09 +0100

    [APP] language card fixes

--- school_data_hub_flutter/lib/common/widgets/dialogs/pupil_list_dialog.dart
+++ school_data_hub_flutter/lib/common/widgets/dialogs/pupil_list_dialog.dart
@@ -3,44 +3,31 @@ import 'package:school_data_hub_flutter/common/theme/app_colors.dart';
 import 'package:school_data_hub_flutter/common/widgets/generic_components/generic_app_bar.dart';
 import 'package:school_data_hub_flutter/common/widgets/generic_components/generic_sliver_list.dart';
 import 'package:school_data_hub_flutter/features/pupil/domain/models/pupil_proxy.dart';
-import 'package:school_data_hub_flutter/features/pupil/presentation/widgets/pupil_list_card.dart';
+import 'package:school_data_hub_flutter/features/pupil/presentation/widgets/pupil_language_card.dart';
 
 class PupilListDialog extends StatelessWidget {
   final String title;
   final List<PupilProxy> pupils;
 
-  const PupilListDialog({
-    super.key,
-    required this.title,
-    required this.pupils,
-  });
+  const PupilListDialog({super.key, required this.title, required this.pupils});
 
   @override
   Widget build(BuildContext context) {
     return Dialog(
       backgroundColor: AppColors.canvasColor,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10),
-      ),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
       child: ConstrainedBox(
-        constraints: const BoxConstraints(
-          maxWidth: 600,
-          maxHeight: 600,
-        ),
+        constraints: const BoxConstraints(maxWidth: 600, maxHeight: 600),
         child: Column(
           children: [
-            GenericAppBar(
-              title: title,
-              iconData: Icons.people,
-            ),
+            GenericAppBar(title: title, iconData: Icons.people),
             Expanded(
               child: CustomScrollView(
                 slivers: [
                   GenericSliverListWithEmptyListCheck(
                     items: pupils,
-                    itemBuilder: (context, pupil) => PupilListCard(
-                      passedPupil: pupil,
-                    ),
+                    itemBuilder: (context, pupil) =>
+                        PupilLanguageCard(passedPupil: pupil),
                   ),
                 ],
               ),
@@ -51,4 +38,3 @@ class PupilListDialog extends StatelessWidget {
     );
   }
 }
-
